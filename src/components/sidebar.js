607c529ee508c6a129be22ba469bc131d8c8ac1a
Alejandro Díaz Vecchio <alejandrod@keoworld.com>
2020-12-02 18:29:24 -0500

    added stuff

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+
+import { UserContext } from '../providers/user-provider'
 
 const MainMenuContainer = styled.div`
   background-color: ${(props) => props.theme.colors.backgroundDark};
@@ -32,28 +34,71 @@ const StyledLink = styled(Link)`
 
 `;
 
+const RolesToString = {
+  ADMIN: 'administrador',
+  PROFESSOR: 'docente',
+  STUDENT: 'estudiantes'
+}
 
 
 const SideBar = (props) => {
+  const user = useContext(UserContext)
+
+  const buildOptions = () => {
+    switch (user.currentRole) {
+      case "ADMIN":
+        return <React.Fragment>
+          <StyledLink className={props.location.pathname === '/solicitudes' ? 'active' : ''} to={props.match.url + 'solicitudes'}>
+            <i className="material-icons-round">question_answer</i>
+            Solicitudes
+          </StyledLink>
+          <StyledLink className={props.location.pathname === '/grupos' ? 'active' : ''} to={props.match.url + 'grupos'}>
+            <i className="material-icons-round">group_work</i>
+            Grupos
+          </StyledLink>
+        </React.Fragment>
+      case "PROFESSOR":
+        return <React.Fragment>
+          <StyledLink className={props.location.pathname === '/grupos' ? 'active' : ''} to={props.match.url + 'grupos'}>
+            <i className="material-icons-round">group_work</i>
+            Grupos
+          </StyledLink>
+          <StyledLink className={props.location.pathname === '/calificacion-docente' ? 'active' : ''} to={props.match.url + 'calificacion-docente'}>
+            <i className="material-icons-round">stars</i>
+            Calificacion Docente
+          </StyledLink>
+          <StyledLink className={props.location.pathname === '/profesor/mis-cursos' ? 'active' : ''} to={props.match.url + 'profesor/mis-cursos'}>
+            <i className="material-icons-round">question_answer</i>
+            Mis cursos
+          </StyledLink>
+        </React.Fragment>
+      case "STUDENT":
+        return <React.Fragment>
+          <StyledLink className={props.location.pathname === '/solicitudes' ? 'active' : ''} to={props.match.url + 'solicitudes'}>
+            <i className="material-icons-round">question_answer</i>
+            Solicitudes
+          </StyledLink>
+          <StyledLink className={props.location.pathname === '/grupos' ? 'active' : ''} to={props.match.url + 'grupos'}>
+            <i className="material-icons-round">group_work</i>
+            Grupos
+          </StyledLink>
+          <StyledLink className={props.location.pathname === '/calificacion-docente' ? 'active' : ''} to={props.match.url + 'calificacion-docente'}>
+            <i className="material-icons-round">stars</i>
+            Calificacion Docente
+          </StyledLink>
+        </React.Fragment>
+      default:
+        break;
+    }
+  }
+
+
+
+
   const buildAdminSideBar = () => {
     return <MainMenuContainer>
-      <h3>Servicios admin</h3>
-      <StyledLink className={props.location.pathname === '/solicitudes' ? 'active' : ''} to={props.match.url + 'solicitudes'}>
-        <i className="material-icons-round">question_answer</i>
-        Solicitudes
-      </StyledLink>
-      <StyledLink  className={props.location.pathname === '/grupos' ? 'active' : ''} to={props.match.url + 'grupos'}>
-        <i className="material-icons-round">group_work</i>
-        Grupos
-      </StyledLink>
-      <StyledLink className={props.location.pathname === '/calificacion-docente' ? 'active' : ''} to={props.match.url + 'calificacion-docente'}>
-        <i className="material-icons-round">stars</i>
-        Calificacion Docente
-      </StyledLink>
-      <StyledLink className={props.location.pathname === '/profesor/mis-cursos' ? 'active' : ''} to={props.match.url + 'profesor/mis-cursos'}>
-        <i className="material-icons-round">question_answer</i>
-        Mis cursos
-      </StyledLink>
+      <h3>Servicios {RolesToString[user.currentRole]}</h3>
+      {buildOptions()}
     </MainMenuContainer>
   }
 
--- src/providers/user-provider.js
+++ src/providers/user-provider.js
@@ -9,16 +9,25 @@ const UserProvider = (props) => {
   const [user, setUser] = useState(undefined);
 
   useEffect(() => {
+    const setCurrentRole = (role) => {
+      setUser((usr) => {
+        const newUser = {...user}
+        newUser.currentRole = role;
+        return newUser
+      })
+    }
+
     auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
+        console.log(userAuth)
         const username = userAuth.email.split('@')[0];
         const usrData = await getUserData(username)
         userAuth.userData = usrData
+        userAuth.currentRole = usrData.roles[0]
       }
       setUser(userAuth);
     });
   }, [])
-
 
   return (
     <UserContext.Provider value={user}>
